456b1e35a8d1991bcf87683135b02bcc0d2b1ea9
Tarric Sookdeo <tarricsookdeo@outlook.com>
2023-03-10 13:24:11 -0500

    Replace generate_random_string function with Django's get_random_string

--- users/views.py
+++ users/views.py
@@ -1,6 +1,4 @@
-import random
-import string
-
+from django.utils.crypto import get_random_string
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -8,10 +6,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import CustomUser
-
-
-def generate_random_string(length: int) -> str:
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
 
 class APIKeysView(APIView):
@@ -26,8 +20,10 @@ class APIKeysView(APIView):
     def post(self, request):
         user = CustomUser.objects.get(id=request.user.id)
         if user:
-            api_key = generate_random_string(32)
-            api_secret = generate_random_string(64)
+            api_key = get_random_string(
+                32, allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+            api_secret = get_random_string(
+                64, allowed_chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
             user.api_key = api_key
             user.api_secret = api_secret
             user.save()
